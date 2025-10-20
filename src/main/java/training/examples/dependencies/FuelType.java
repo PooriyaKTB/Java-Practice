c49cfc735aec75f25b590c095419dad6b4fcb670
Pooriya Ketabi <159670309+PooriyaKTB@users.noreply.github.com>
2025-10-20 13:08:17 +0100

    Initial

---
+++ src/main/java/training/examples/dependencies/FuelType.java
@@ -0,0 +1,6 @@
+package training.examples.dependencies;
+
+/**
+ * This will be a class representing a fuel type - Have a go at writing it from scratch.
+ * This class will have one property that holds the name of the fuel being used.
+ */
---
+++ src/main/java/training/exercises/exercise4/WeeklyRentalContractMaker.java
@@ -0,0 +1,21 @@
+package training.exercises.exercise4;
+
+/**
+ * Class that helps us make a weekly contract
+ */
+class WeeklyRentalContractMaker {
+
+
+  /**
+   * Creates a 'standard' contract where the customer pays every Friday.
+   *
+   * @param customerName the name of the customer
+   * @param weeklyPaymentAmount the weekly payment amount in pounds
+   * @return a weekly rental contract with the given customer and payment amount and a payment
+   * day of the week of Friday.
+   */
+  WeeklyRentalContract createStandardContract(String customerName, int weeklyPaymentAmount) {
+    // TODO create the standard contract
+    return null;
+  }
+}
