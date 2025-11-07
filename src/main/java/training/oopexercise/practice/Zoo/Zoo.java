a57dc7a875f7ce7ab30352832a0436547e2fb7c4
Pooriya <pooriya.ketabi@gmail.com>
2025-11-07 21:40:12 +0000

    practice

---
+++ src/main/java/training/oopexercise/practice/Zoo/Zoo.java
@@ -0,0 +1,10 @@
+package training.oopexercise.practice.Zoo;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Animal dog = new Animal("Chico", 3, 18);
+        System.out.println(dog.name);
+        dog.sleep();
+        dog.eat();
+    }
+}
