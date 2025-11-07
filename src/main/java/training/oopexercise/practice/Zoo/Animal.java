148a84213523efac85b01362cca1fbd265b9b9e1
Pooriya <pooriya.ketabi@gmail.com>
2025-11-07 21:30:55 +0000

    practice

---
+++ src/main/java/training/oopexercise/practice/Zoo/Animal.java
@@ -0,0 +1,21 @@
+package training.oopexercise.practice.Zoo;
+
+public class Animal {
+    String name;
+    int age;
+    int weight;
+
+    public Animal(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+}
---
+++ src/main/java/training/oopexercise/practice/Zoo/Bird.java
@@ -0,0 +1,11 @@
+package training.oopexercise.practice.Zoo;
+
+public class Bird extends Animal{
+    public Bird(String name, int age, int weight) {
+        super(name, age, weight);
+    }
+
+    public void fly(){
+        System.out.println(name + " is flying");
+    }
+}
