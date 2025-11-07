f0797d7ec291afa1e966f55bf807dceffb8258ec
Pooriya <pooriya.ketabi@gmail.com>
2025-11-07 00:47:39 +0000

    practice

--- src/main/java/training/oopexercise/practice/Main.java
+++ src/main/java/training/oopexercise/practice/Main.java
@@ -1,14 +1,17 @@
 package training.oopexercise.practice;
 
-
 public class Main {
     public static void main(String[] args) {
-        needs("pooriya", "fereshteh", "Nathan");
+        avg(10, 20, 30, 40, 50);
     }
 
-    public static void needs(String ... needs) {
-         for (String need : needs){
-             System.out.println(need);
-         }
+    public static void avg(int... grades) {
+        int avg = 0;
+        for (int grade : grades) {
+            avg += grade;
+        }
+        System.out.println(avg);
+        System.out.println(avg / grades.length);
+
     }
 }
