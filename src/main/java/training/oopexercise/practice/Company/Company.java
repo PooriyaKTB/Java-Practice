9db2abd606f9544ad2a0d5b4e0b214af971b8a17
Pooriya <pooriya.ketabi@gmail.com>
2025-11-08 22:57:21 +0000

    practice

--- src/main/java/training/oopexercise/practice/Company/Company.java
+++ src/main/java/training/oopexercise/practice/Company/Company.java
@@ -2,27 +2,21 @@ package training.oopexercise.practice.Company;
 
 public class Company {
     public static void main(String[] args) {
-        System.out.println("--CEO--");
+
         CEO ceo = new CEO();
-        ceo.salaryGet();
-
-        System.out.println("--CTO--");
-        CTO cto = new CTO();
-        cto.salaryGet();
-
-        System.out.println("--CFO--");
-        CFO cfo = new CFO();
-        cfo.salaryGet();
-
-        System.out.println("--Manager--");
-        Manager manager = new Manager();
-        manager.salaryGet();
-
-        System.out.println("--Staff--");
-        Staff staff = new Staff();
-        staff.salaryGet();
-
         paySalary(ceo);
+
+        CTO cto = new CTO();
+        paySalary(cto);
+
+        CFO cfo = new CFO();
+        paySalary(cfo);
+
+        Manager manager = new Manager();
+        paySalary(manager);
+
+        Staff staff = new Staff();
+        paySalary(staff);
     }
 
     public static void paySalary(Employee role) {
